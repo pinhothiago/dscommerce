4f25da48489e45aa32e3b74f97f5504beeb20314
Thiago Pinho <thiagopinho05@gmail.com>
2025-06-22 11:18:57 -0300

    feat: get all products paged

--- src/main/java/com/pinhothiago/dscommerce/services/ProductService.java
+++ src/main/java/com/pinhothiago/dscommerce/services/ProductService.java
@@ -4,6 +4,8 @@ import com.pinhothiago.dscommerce.dto.ProductDTO;
 import com.pinhothiago.dscommerce.entities.Product;
 import com.pinhothiago.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,5 +23,11 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(ProductDTO::new);
     }
 }
