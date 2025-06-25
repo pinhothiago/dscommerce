123f4351914504f74a142d87a1bd7625f07796e8
Thiago Pinho <thiagopinho05@gmail.com>
2025-06-24 22:48:23 -0300

    feat: creating method POST to add products on API

--- src/main/java/com/pinhothiago/dscommerce/services/ProductService.java
+++ src/main/java/com/pinhothiago/dscommerce/services/ProductService.java
@@ -30,4 +30,17 @@ public class ProductService {
         Page<Product> result = repository.findAll(pageable);
         return result.map(ProductDTO::new);
     }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setPrice(dto.getPrice());
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
+     }
 }
