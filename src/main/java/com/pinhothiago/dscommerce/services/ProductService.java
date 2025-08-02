9a0a300b78e4bc15c0cbb4af770be1be5405f9e0
Thiago Pinho <thiagopinho05@gmail.com>
2025-08-02 20:44:27 -0300

    feat: creating deleting product method

--- src/main/java/com/pinhothiago/dscommerce/services/ProductService.java
+++ src/main/java/com/pinhothiago/dscommerce/services/ProductService.java
@@ -47,6 +47,12 @@ public class ProductService {
         return new ProductDTO(entity);
     }
 
+    @Transactional
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+
     private void copyDTOtoEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
